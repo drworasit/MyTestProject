a4d248e1efc2d3a01b769a6001a2b3431fc9fb66
drworasit <worasit@cbs.chula.ac.th>
2023-01-23 18:16:05 +0700

    Initial Commit

---
+++ MyTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTestProject
+//
+//  Created by DrKeng on 23/1/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
